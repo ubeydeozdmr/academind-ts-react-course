01e3ac7724191f52a296d1cbc3a79700d1d91965
Ubeyde Emir Özdemir <ubeydeozdmr@gmail.com>
2024-08-12 17:43:32 +0300

    Merging Types

--- first-app.ts
+++ first-app.ts
@@ -85,3 +85,37 @@ function login(credentials: Credentials): void {
 }
 
 login(new AuthCredentials());
+
+// type Admin = {
+//   permissions: string[];
+// };
+
+// type AppUser = {
+//   userName: string;
+// };
+
+// type AppAdmin = Admin & AppUser;
+
+// let admin: AppAdmin;
+
+// admin = {
+//   permissions: ['READ', 'WRITE'],
+//   userName: 'Ubeyde',
+// };
+
+interface Admin {
+  permissions: string[];
+}
+
+interface AppUser {
+  userName: string;
+}
+
+interface AppAdmin extends Admin, AppUser {}
+
+let admin: AppAdmin;
+
+admin = {
+  permissions: ['READ', 'WRITE'],
+  userName: 'Ubeyde',
+};
