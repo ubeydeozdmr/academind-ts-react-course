c6729c8c6380dc17c58ac46f87b0c50030ed6e16
Ubeyde Emir Özdemir <ubeydeozdmr@gmail.com>
2024-08-12 15:39:14 +0300

    Working With Object Types

--- first-app.ts
+++ first-app.ts
@@ -13,3 +13,22 @@ let userID: string | number = 'abc1';
 userID = 123;
 
 // userID = true;
+
+// let user: object;
+let user: {
+  name: string;
+  age: number;
+  isAdmin: boolean;
+  id: string | number;
+};
+
+// user = 'Ubeyde';
+
+user = {
+  name: 'Ubeyde',
+  age: 22,
+  isAdmin: true,
+  id: 'abc', // 123
+};
+
+// user = {};
