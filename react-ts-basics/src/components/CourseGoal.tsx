0317dc83d78f1dac85aabf37b3e7598bc6ac41c7
Ubeyde Emir Özdemir <ubeydeozdmr@gmail.com>
2024-08-13 00:33:40 +0300

    Storing Props Types as a Custom Type or Interface

--- react-ts-basics/src/components/CourseGoal.tsx
+++ react-ts-basics/src/components/CourseGoal.tsx
@@ -1,10 +1,9 @@
-export default function CourseGoal({
-  title,
-  description,
-}: {
+interface CourseGoalProps {
   title: string;
   description: string;
-}) {
+}
+
+export default function CourseGoal({ title, description }: CourseGoalProps) {
   return (
     <article>
       <div>
