f9380910bb0377a435e7e8dae4d5f41727a778c2
Ubeyde Emir Özdemir <ubeydeozdmr@gmail.com>
2024-08-12 17:50:58 +0300

    Adding Type Guards

--- first-app.ts
+++ first-app.ts
@@ -120,9 +120,17 @@ admin = {
   userName: 'Ubeyde',
 };
 
-let role: 'admin' | 'user' | 'editor'; // 'admin', 'user', 'editor'
+type Role = 'admin' | 'user' | 'editor';
+
+let role: Role; // 'admin', 'user', 'editor'
 
 role = 'admin';
 role = 'user';
 role = 'editor';
 // role = 'abc';
+
+function performAction(action: string | number, role: Role) {
+  if (role === 'admin' && typeof action === 'string') {
+    // ...
+  }
+}
