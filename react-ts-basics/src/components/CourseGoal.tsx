18e7639b8b727716f642f4e57f484bf3c4470c54
Ubeyde Emir Özdemir <ubeydeozdmr@gmail.com>
2024-08-13 22:32:58 +0300

    Another Way Of Typing Components

--- react-ts-basics/src/components/CourseGoal.tsx
+++ react-ts-basics/src/components/CourseGoal.tsx
@@ -18,3 +18,17 @@ export default function CourseGoal({ title, children }: CourseGoalProps) {
     </article>
   );
 }
+
+// const CourseGoal: FC<CourseGoalProps> = ({ title, children }) => {
+//   return (
+//     <article>
+//       <div>
+//         <h2>{title}</h2>
+//         {children}
+//       </div>
+//       <button>Delete</button>
+//     </article>
+//   );
+// };
+
+// export default CourseGoal;
