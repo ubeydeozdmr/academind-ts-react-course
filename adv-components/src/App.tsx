b306e5dd91a6e51b7408bddae44e0237a57e3e70
Ubeyde Emir Özdemir <ubeydeozdmr@gmail.com>
2024-08-14 23:36:08 +0300

    Working with Type Predicates & Facing TypeScript Limitations

--- adv-components/src/App.tsx
+++ adv-components/src/App.tsx
@@ -4,14 +4,10 @@ function App() {
   return (
     <main>
       <p>
-        <Button el="button" type="button">
-          A Button
-        </Button>
+        <Button>A Button</Button>
       </p>
       <p>
-        <Button el="anchor" href="https://example.com">
-          An Anchor
-        </Button>
+        <Button href="https://example.com">An Anchor</Button>
       </p>
     </main>
   );
