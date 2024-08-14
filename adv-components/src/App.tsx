ad74c54ebe9bcac1c4fadc644134463755c21dcd
Ubeyde Emir Özdemir <ubeydeozdmr@gmail.com>
2024-08-14 23:42:51 +0300

    Building a Basic Polymorphic Component

--- adv-components/src/App.tsx
+++ adv-components/src/App.tsx
@@ -1,14 +1,10 @@
 import Button from './components/Button';
+import Container from './components/Container';
 
 function App() {
   return (
     <main>
-      <p>
-        <Button>A Button</Button>
-      </p>
-      <p>
-        <Button href="https://example.com">An Anchor</Button>
-      </p>
+      <Container as={Button} />
     </main>
   );
 }
