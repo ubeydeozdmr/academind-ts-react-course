aa9bf7992b5e2cdf7dcc41e25796db4a11b7ba56
Ubeyde Emir Özdemir <ubeydeozdmr@gmail.com>
2024-08-15 00:09:52 +0300

    Building Another Wrapper Component (Custom Form Component)

--- adv-components/src/App.tsx
+++ adv-components/src/App.tsx
@@ -1,12 +1,17 @@
-import { useRef } from 'react';
+import Button from './components/Button';
+import Form from './components/Form';
 import Input from './components/Input';
 
 function App() {
-  const input = useRef<HTMLInputElement>(null);
-
   return (
     <main>
-      <Input label="Test" id="test" ref={input} />
+      <Form>
+        <Input type="text" label="Name" id="name" />
+        <Input type="number" label="Age" id="age" />
+        <p>
+          <Button>Save</Button>
+        </p>
+      </Form>
     </main>
   );
 }
