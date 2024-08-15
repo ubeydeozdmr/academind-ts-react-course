464a215d85b86dac6f682af8d1d577d2aca3e541
Ubeyde Emir Özdemir <ubeydeozdmr@gmail.com>
2024-08-15 12:45:01 +0300

    Creating a Type-Safe Provider Component

--- adv-components/src/store/timers-context.tsx
+++ adv-components/src/store/timers-context.tsx
@@ -1,4 +1,4 @@
-import { createContext } from 'react';
+import { type ReactNode, createContext } from 'react';
 
 type Timer = {
   name: string;
@@ -17,3 +17,22 @@ type TimersContextValue = TimersState & {
 };
 
 const TimersContext = createContext<TimersContextValue | null>(null);
+
+type TimersContextProviderProps = {
+  children: ReactNode;
+};
+
+export default function TimersContextProvider({
+  children,
+}: TimersContextProviderProps) {
+  const ctx: TimersContextValue = {
+    timers: [],
+    isRunning: false,
+    addTimer: (timerData) => {},
+    startTimers: () => {},
+    stopTimers: () => {},
+  };
+  return (
+    <TimersContext.Provider value={ctx}>{children}</TimersContext.Provider>
+  );
+}
