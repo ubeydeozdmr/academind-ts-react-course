9868cf05efe90f0a7cabf79365c156b3de04be0b
Ubeyde Emir Özdemir <ubeydeozdmr@gmail.com>
2024-08-15 23:35:13 +0300

    Finishing Touches & Summary

--- redux/src/components/CartItems.tsx
+++ redux/src/components/CartItems.tsx
@@ -1,9 +1,34 @@
+import {
+  type CartItem,
+  addToCart,
+  removeFromCart,
+} from '../store/cart-slice.ts';
+import { useCartDispatch, useCartSelector } from '../store/hooks.ts';
+
 export default function CartItems() {
+  const cartItems = useCartSelector((state) => state.cart.items);
+  const dispatch = useCartDispatch();
+
+  const totalPrice = cartItems.reduce(
+    (val, item) => val + item.price * item.quantity,
+    0,
+  );
+  const formattedTotalPrice = totalPrice.toFixed(2);
+
+  function handleAddToCart(item: CartItem) {
+    dispatch(addToCart(item));
+  }
+
+  function handleRemoveFromCart(id: string) {
+    dispatch(removeFromCart(id));
+  }
+
   return (
     <div id="cart">
-      <p>No items in cart!</p>
+      {cartItems.length === 0 && <p>No items in cart!</p>}
 
-      {/* <ul id="cart-items">
+      {cartItems.length > 0 && (
+        <ul id="cart-items">
           {cartItems.map((item) => {
             const formattedPrice = `$${item.price.toFixed(2)}`;
 
@@ -23,11 +48,12 @@ export default function CartItems() {
               </li>
             );
           })}
-        </ul> */}
+        </ul>
+      )}
 
-      {/* <p id="cart-total-price">
-        Cart Total: <strong>{formattedTotalPrice}</strong>
-      </p> */}
+      <p id="cart-total-price">
+        Cart Total: <strong>${formattedTotalPrice}</strong>
+      </p>
     </div>
   );
 }
