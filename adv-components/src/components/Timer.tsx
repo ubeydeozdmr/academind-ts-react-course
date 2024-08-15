7773e40d15911a3ffe539d5157d62b7fcfd15290
Ubeyde Emir Özdemir <ubeydeozdmr@gmail.com>
2024-08-15 17:36:38 +0300

    Using useEffect() with TypeScript

--- adv-components/src/components/Timer.tsx
+++ adv-components/src/components/Timer.tsx
@@ -1,22 +1,26 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import Container from './UI/Container.tsx';
 import { type Timer as TimerProps } from '../store/timers-context.tsx';
 
 export default function Timer({ name, duration }: TimerProps) {
-  const [remainingState, setRemainingTime] = useState(duration * 1000);
+  const [remainingTime, setRemainingTime] = useState(duration * 1000);
 
-  setInterval(function () {
-    setRemainingTime((prevTime) => prevTime - 50);
-  }, 50);
+  useEffect(() => {
+    setInterval(function () {
+      setRemainingTime((prevTime) => prevTime - 50);
+    }, 50);
+  }, []);
+
+  const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
 
   return (
     <Container as="article">
       <h2>{name}</h2>
       <p>
-        <progress max={duration * 1000} value={remainingState} />
+        <progress max={duration * 1000} value={remainingTime} />
       </p>
-      <p>{}</p>
+      <p>{formattedRemainingTime}</p>
     </Container>
   );
 }
