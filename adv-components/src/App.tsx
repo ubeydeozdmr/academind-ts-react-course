f41278ad4e5d69ded764508a01507528602ccdd6
Ubeyde Emir Özdemir <ubeydeozdmr@gmail.com>
2024-08-15 10:45:10 +0300

    The Starting Project: before starting to Section 5

--- adv-components/src/App.tsx
+++ adv-components/src/App.tsx
@@ -1,27 +1,16 @@
-import { useRef } from 'react';
-import Button from './components/Button';
-import Form, { type FormHandle } from './components/Form';
-import Input from './components/Input';
+import AddTimer from './components/AddTimer.tsx';
+import Header from './components/Header.tsx';
+import Timers from './components/Timers.tsx';
 
 function App() {
-  const customForm = useRef<FormHandle>(null);
-
-  function handleSave(data: unknown) {
-    const extractedData = data as { name: string; age: string };
-    console.log(extractedData);
-    customForm.current?.clear();
-  }
-
   return (
-    <main>
-      <Form onSave={handleSave}>
-        <Input type="text" label="Name" id="name" />
-        <Input type="number" label="Age" id="age" />
-        <p>
-          <Button>Save</Button>
-        </p>
-      </Form>
-    </main>
+    <>
+      <Header />
+      <main>
+        <AddTimer />
+        <Timers />
+      </main>
+    </>
   );
 }
 
